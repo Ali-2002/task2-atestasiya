cbef32f020caab929a0df788fa38446376b91790
Ali-2002 <ali.aliquliyev@inbox.ru>
2022-11-14 18:01:41 +0400

    atestasiya-task2

---
+++ main.js
@@ -0,0 +1,37 @@
+class Message {
+    constructor(author, time, text) {
+      this.author = author;
+      this.time=time
+      this.text = text;
+    }
+    toString() {
+        return `${this.time} ${this.author}: ${this.text}`;
+    }
+}
+
+class Messenger {
+    constructor() {
+      this.arr = [];
+    }
+  
+    show_history() {
+      this.arr.forEach((item) => {
+        console.log(item.toString());
+      });
+    }
+  
+    send(author, text) {
+      this.arr.push(new Message(author, this.gettime(), text));
+    }
+  
+    gettime() {
+      let now = new Date();
+      return `${now.getHours()}:${now.getMinutes()}`;
+    }
+  }
+
+  let messenger = new Messenger();
+  messenger.send("Adil", "ilk mesaj");
+  messenger.send("Məryəm", "İkinci mesaj");
+  messenger.show_history();
+  
